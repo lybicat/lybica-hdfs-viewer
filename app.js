276745fcb9150f95a5d51e7a74714dfc366864e0
Zhang Yu <feiyuw@gmail.com>
2015-11-15 09:57:17 +0800

    use 2 space as tab indention

--- app.js
+++ app.js
@@ -2,8 +2,8 @@ var restify = require('restify');
 var config = require('./config');
 
 var server = restify.createServer({
-    name: 'hdfs-viewer',
-    version: '1.0.0'
+  name: 'hdfs-viewer',
+  version: '1.0.0'
 });
 
 server.use(restify.acceptParser(server.acceptable));
@@ -11,8 +11,8 @@ server.use(restify.queryParser());
 server.use(restify.bodyParser());
 
 server.use(function(req, res, next) {
-    res.header('Access-Control-Allow-Origin', '*');
-    return next();
+  res.header('Access-Control-Allow-Origin', '*');
+  return next();
 });
 
 
@@ -23,5 +23,5 @@ server.get('/hdfs/:filePath', function(req, res, next) {
 
 
 server.listen(config.PORT, function() {
-    console.log('%s listening at %s', server.name, server.url);
+  console.log('%s listening at %s', server.name, server.url);
 });
